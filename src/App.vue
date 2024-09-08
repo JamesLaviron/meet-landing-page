deccfe87828f5e110ad6b7f867fad236ff8173af
JamesLaviron <victor@gerfaud.fr>
2024-09-08 18:19:50 +0200

    Init project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <img
+      src="../images/logo-118x28.png"
+      alt="logo"
+      aria-label="Meet logo"
+      class="header__logo"
+      width="118"
+      height="28"
+    />
+  </header>
+  <body>
+    <header></header>
+    <section></section>
+  </body>
+  <footer></footer>
+</template>
+<script>
+import "./assets/styles/main.scss";
+export default {
+  name: "App",
+  components: {},
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/meet-landing-page/" : "/",
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/styles/_reset.scss";
+          @import "@/assets/styles/base.scss";
+          @import "@/assets/styles/_variables.scss";
+          @import "@/assets/styles/_utilities.scss";
+          @import "@/assets/styles/_mixins.scss";
+          @import "@/assets/styles/text-presets.scss";
+          @import "@/assets/styles/common.scss";
+        `
+      }
+    }
+  }
+})
